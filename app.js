866225cef77af533cfddad0e3d9d2a4361e3670d
gsanchez13 <gisellesanchez@pursuit.org>
2019-10-15 19:40:11 -0400

    DOM manipulation for number request

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const getAnimal = async () => {
     let url = `http://localhost:8080/animal/${input.value}`;
     let animalObj = await axios.get(url).then((response => {return response.data}));
     let ifExists = animalObj.message;
-        ifExists ? weHaveIt(animalObj) : notHere(animalObj);
+        ifExists ? foundAnimal(animalObj) : unfoundAnimal(animalObj);
 }
-const weHaveIt = (object) => {
+const foundAnimal = (object) => {
     let container = document.querySelector("#container");
     let paragraph = document.querySelector("p");
     let para = paragraph ? paragraph : createPara();
@@ -14,14 +14,14 @@ const weHaveIt = (object) => {
         para.innerText = `${result}!`
         container.append(para)
 }
-const notHere = (object) => {
+const unfoundAnimal = (object) => {
     let container = document.querySelector("#container");
-    let paragraph = document.querySelector("p");
-    let para = paragraph ? paragraph : createPara();
-    console.log(object)
+    let paragraph = document.querySelector("#animalPara");
+    let animalPara = paragraph ? paragraph : createPara();
+        animalPara.id = "animalPara"
     let result = object.status.toUpperCase();
-        para.innerText = `${result}!`
-        container.append(para)
+        animalPara.innerText = `${result}!`
+        container.append(animalPara)
 }
 const createPara = () => {
     let paragraph = document.createElement("p");
@@ -32,5 +32,23 @@ const getRandomNumber = async () => {
     let ceiling = document.querySelector("#ceiling");
     let url = `http://localhost:8080/random?num1=${floor.value}&num2=${ceiling.value}`
     let numObj = await axios.get(url).then((response)=> {return response.data})
-    console.log(numObj)
+    numObj.message ? showNum(numObj) : showRejection(numObj)
+}
+const showNum = (obj) => {
+    let numDiv = document.querySelector("#question_2")
+    let paragraph = document.querySelector("#numPara");
+    let numPara = paragraph ? paragraph : createPara();
+        numPara.id="numPara";
+    let answer = obj.message;
+        numPara.innerText = answer;
+        numDiv.append(numPara);
+}
+const showRejection = (obj) => {
+    let numDiv = document.querySelector("#question_2")
+    let paragraph = document.querySelector("#numPara");
+    let numPara = paragraph ? paragraph : createPara();
+        numPara.id="numPara";
+    let answer = obj.message;
+        numPara.innerText = answer;
+        numDiv.append(numPara);
 }
